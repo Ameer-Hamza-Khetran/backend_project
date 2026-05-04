d54634d593b64f746f29eb228cb9d6db6314a9ff
Ameer Hamza Khetran <hamzaa803@gmail.com>
2026-05-04 22:25:21 +0500

    utils files creation

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+/** TRY CATCH HIGHER ORDER FUNCTION FOR FUNCTION EXECUTION */
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
